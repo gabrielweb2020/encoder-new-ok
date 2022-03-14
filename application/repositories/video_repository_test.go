3b0e7fd7c7eb34c6e2bfb02245e9b1a04099bbbd
Gabriel Bittencourt <gabriel.bc88@gmail.com>
2022-03-14 20:40:47 -0300

    add video and job repositories

---
+++ application/repositories/video_repository_test.go
@@ -0,0 +1,30 @@
+package repositories_test
+
+import (
+	"encoder/domain"
+	"encoder/framework/database"
+	"testing"
+	"time"
+
+	uuid "github.com/satori/go.uuid"
+	"github.com/stretchr/testify/require"
+)
+
+func TestVideoRepositoryDbInsert(t *testing.T) {
+	db := database.NewDbTest()
+	defer db.Close()
+
+	video := domain.NewVideo()
+	video.ID = uuid.NewV4().String()
+	video.FilePath = "path"
+	video.CreatedAt = time.Now()
+
+	repo := repositories.VideoRepositoryDB{Db: db}
+	repo.Insert(video)
+
+	v, err := repo.Find(video.ID)
+
+	require.NotEmpty(t, v.ID)
+	require.Nil(t, err)
+	require.Equal(t, v.ID, video.ID)
+}
